5e9b7e1e6463de71a020410435a3d4b5f0fc6e79
Ben Nathanson <benedwinnathanson@gmail.com>
2020-09-22 14:11:29 -0400

    Updates

---
+++ precache-manifest.f4f1024c2b2c32c13ee238680785fdac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaabc48e4323c969a8574034fed8cf0",
+    "url": "/events/index.html"
+  },
+  {
+    "revision": "af886ef5c4855a804981",
+    "url": "/events/static/css/2.3e9ef898.chunk.css"
+  },
+  {
+    "revision": "3383d854cd946b306424",
+    "url": "/events/static/css/main.61e0a910.chunk.css"
+  },
+  {
+    "revision": "af886ef5c4855a804981",
+    "url": "/events/static/js/2.ffd94cca.chunk.js"
+  },
+  {
+    "revision": "6ca09d2a4d3a38ef49e84ea4141e08de",
+    "url": "/events/static/js/2.ffd94cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3383d854cd946b306424",
+    "url": "/events/static/js/main.8fed9b24.chunk.js"
+  },
+  {
+    "revision": "5b58ad38169992d3a40e",
+    "url": "/events/static/js/runtime-main.15b6f00f.js"
+  },
+  {
+    "revision": "0d6bbdaf2a0072f6e26583a1ab836c76",
+    "url": "/events/static/media/logo.0d6bbdaf.png"
+  }
+]);
